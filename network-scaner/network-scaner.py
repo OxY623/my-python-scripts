0ddd4b69b0b6598cb9b564ad3058011225f9bf6d
oxy <fuzzydoorproductions0@gmail.com>
2026-01-02 00:57:14 +0300

    fix: refactored script scaner netwjork

--- network-scaner/network-scaner.py
+++ network-scaner/network-scaner.py
@@ -1,16 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import scapy.all as scapy
 
-
-
 def scan(ip):
-    # answered, unanswered = scapy.arping(ip, verbose=False)
-    # return answered
-    arp_request = scapy.ARP()
-    print(arp_request.summary())
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_broadcast = broadcast / arp_request
+
+    answered_list = scapy.srp(
+        arp_req_broadcast,
+        timeout=1,
+        verbose=False
+    )[0]
+
+    clients_list = []
+
+    for idx, el in enumerate(answered_list):
+        client = {
+            "id": idx,
+            "ip": el[1].psrc,
+            "mac": el[1].hwsrc
+        }
+        clients_list.append(client)
+
+       
+
+    return clients_list
+
+def print_result(data):
+    print("id\tIP\t\t\tMAC Address")
+    print("---------------------------------------------------")
+    for el in data:
+        print(f"{el['id']}\t{el['ip']}\t\t{el['mac']}")
+        print("---------------------------------------------------")
+
 
 if __name__ == "__main__":
     result = scan("192.168.192.1/24")
-    for sent, received in result:
-        print(received.psrc, received.hwsrc)
-
+    print_result(result)
